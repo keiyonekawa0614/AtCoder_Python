a93c010e873bc7fc09bea8900ad9eebda748df78
Kei Yonekawa <kei.yonekawa0614@gmail.com>
2021-05-12 23:46:04 +0900

    add: ABC199 B.py

---
+++ ABC/ABC199/B.py
@@ -0,0 +1,14 @@
+n = int(input())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+res = 0
+for i in range(1, 1001):
+  ok = True
+  for j in range(n):
+    if i < a[j] or i > b[j]:
+      ok = False
+  if ok:
+    res += 1
+
+print(res)
